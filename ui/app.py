9aac5aa2b3735fcaf52f99dc9440558607202a2c
Kamil Dzierzak <kamdzierzak@gmail.com>
2024-12-31 18:22:29 +0100

    feat: connect model to streamlit app

--- ui/app.py
+++ ui/app.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import numpy as np
 
-# Konfiguracja strony
-st.set_page_config(page_title="Rozpoznawanie Konstelacji", page_icon="✨", layout="centered")
+@st.cache_resource
+def load_model():
+    model = tf.keras.models.load_model("models/cnn_model.h5")
+    # model = tf.keras.models.load_model("/models/mobilenetv2_model.h5")
+    return model
+
+def preprocess_image(raw_img):
+    img = raw_img.resize((224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0
+    return img_array
 
 # Styl CSS dla tła
 def add_background_image(image_url: str):
@@ -20,6 +33,11 @@ def add_background_image(image_url: str):
     )
 
 def main():
+    # Konfiguracja strony
+    st.set_page_config(page_title="Rozpoznawanie Konstelacji", page_icon="✨", layout="centered")
+
+    model = load_model()
+    
     # Dodanie obrazu tła
     add_background_image("https://static.vecteezy.com/system/resources/thumbnails/019/856/500/small_2x/motion-of-shinny-stars-animation-on-black-background-night-stars-skies-with-twinkling-or-blinking-stars-motion-background-looping-seamless-space-backdrop-travel-free-video.jpg")
 
@@ -40,10 +58,19 @@ def main():
         # Wyświetlenie przesłanego obrazu
         st.image(uploaded_file, caption="Przesłane zdjęcie", use_container_width=True)
 
+        # Przetworzenie obrazu do formatu zgodnego z modelem
+        img = image.load_img(uploaded_file)
+        img_array = preprocess_image(img)
+
         # Przycisk do uruchomienia przewidywania (logika backendu do dodania później)
         if st.button("Rozpoznaj konstelację"):
-            # TODO: Tutaj podłącz backend modelu do przewidywań
-            prediction_placeholder.subheader("Przewidywanie: ⏳ Model w trakcie implementacji...")
+            predictions = model.predict(img_array)
+            predicted_class = np.argmax(predictions, axis=1)
+
+            class_names = ['Andromeda', 'Antlia', 'Apus', 'Aquarius', 'Aquila', 'Ara', 'Aries', 'Auriga', 'Bootes', 'Caelum', 'Camelopardalis', 'Cancer', 'Canes Venatici', 'Canis Maior', 'Canis Minor', 'Capricornus', 'Carina', 'Cassiopeia', 'Centaurus', 'Cepheus', 'Cetus', 'Chamaeleon', 'Circinus', 'Columba', 'Coma Berenices', 'Corona Australis', 'Corona Borealis', 'Corvus', 'Crater', 'Crux', 'Cygnus', 'Delphinus', 'Dorado', 'Draco', 'Equuleus', 'Eridanus', 'Fornax', 'Gemini', 'Grus', 'Hercules', 'Horologium', 'Hydra', 'Hydrus', 'Indus', 'Lacerta', 'Leo', 'Leo Minor', 'Lepus', 'Libra', 'Lupus', 'Lynx', 'Lyra', 'Mensa', 'Microscopium', 'Monoceros', 'Musca', 'Norma', 'Octans', 'Ophiuchus', 'Orion', 'Pavo', 'Pegasus', 'Perseus', 'Phoenix', 'Pictor', 'Pisces', 'Piscis Austrinus', 'Puppis', 'Pyxis', 'Reticulum', 'Sagitta', 'Sagittarius', 'Scorpius', 'Sculptor', 'Scutum', 'Serpens', 'Sextans', 'Taurus', 'Telescopium', 'Triangulum', 'Triangulum Australe', 'Tucana', 'Ursa Maior', 'Ursa Minor', 'Vela', 'Virgo', 'Volans', 'Vulpecula']
+            predicted_label = class_names[predicted_class[0]]
+
+            prediction_placeholder.subheader(f"Przewidywanie: {predicted_label}")
         else:
             prediction_placeholder.subheader("Kliknij przycisk, aby uruchomić przewidywanie.")
 
